e04aafea49cf4610516a306118b6a7f796fa6727
Valensio Deva <devalensio@gmail.com>
2021-05-09 15:51:19 +0700

    use DropdownButtonFormField instead of FormField only

--- lib/widgets/dropdown.dart
+++ lib/widgets/dropdown.dart
@@ -4,47 +4,44 @@ class Dropdown extends StatelessWidget {
   String placeholder;
   String value;
   List<String> options;
+  Function onChanged;
 
   Dropdown({
     @required this.placeholder,
     @required this.value,
     @required this.options,
+    @required this.onChanged,
   });
 
   @override
   Widget build(BuildContext context) {
-    return FormField<String>(
-      builder: (FormFieldState<String> state) {
-        return InputDecorator(
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.grey[200],
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide.none,
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: Colors.grey[400]),
-            ),
-          ),
-          child: DropdownButtonHideUnderline(
-            child: DropdownButton<String>(
-              hint: Text(placeholder),
-              value: value,
-              isDense: true,
-              onChanged: (newValue) {},
-              dropdownColor: Colors.white,
-              items: options.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-          ),
-        );
-      },
+    return DropdownButtonFormField(
+      dropdownColor: Colors.white,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: Colors.grey[400]),
+        ),
+      ),
+      value: value,
+      hint: Text(placeholder),
+      onChanged: onChanged,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: (value) => value == null ? 'Field cannot be empty' : null,
+      items: options.map<DropdownMenuItem<String>>(
+        (String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        },
+      ).toList(),
     );
   }
 }
