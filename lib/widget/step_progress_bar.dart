e550d71e2c8775ccac213e656188d76308712df7
Valensio Deva <devalensio@gmail.com>
2021-05-09 11:02:16 +0700

    construct stepper design

--- lib/widget/step_progress_bar.dart
+++ lib/widget/step_progress_bar.dart
@@ -17,9 +17,51 @@ class StepProgressBar extends StatelessWidget {
     @required this.inactiveColor,
   });
 
+  List<Widget> _stepView() {
+    var list = <Widget>[];
+    contents.asMap().forEach((i, content) {
+      list.add(
+        Container(
+          width: 45.0,
+          height: 45.0,
+          padding: EdgeInsets.all(0),
+          alignment: Alignment.center,
+          child: Text(
+            (i + 1).toString(),
+            style: TextStyle(fontSize: 18),
+          ),
+          decoration: BoxDecoration(
+            color: content.isActive ? activeColor : inactiveColor,
+            borderRadius: BorderRadius.all(
+              Radius.circular(25.0),
+            ),
+            border: Border.all(
+              color: Colors.black,
+              width: 1.0,
+            ),
+          ),
+        ),
+      );
+
+      //line between icons
+      if (i != contents.length - 1) {
+        list.add(
+          Expanded(
+            child: Container(
+              height: 3.0,
+              color: Colors.black,
+            ),
+          ),
+        );
+      }
+    });
+
+    return list;
+  }
+
   Widget build(BuildContext context) {
     return Row(
-      children: [],
+      children: _stepView(),
     );
   }
 }
